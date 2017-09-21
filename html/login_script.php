a9452bb1142797701f73e39141a5ee898c488647
TheWussPuss <kapsmudit@gmail.com>
2017-09-21 23:03:47 +0530

    Aligned login page

--- html/login_script.php
+++ html/login_script.php
@@ -3,19 +3,34 @@
 </head>
 <body>
 <?php
-echo "opened<br/>";
 $servername = "localhost";
 $username = "root";
 $password = "root";
+$database = "vitpay";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
+echo "Connected successfully";
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
+$sql = "SELECT * FROM User;";
+$result = $conn->query($sql);
+echo "<br>";
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "FirstName: " . $row["FirstName"]. " - LastName: " . $row["LastName"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+
+$conn->close();
 ?>		
 </body>
 </html>
